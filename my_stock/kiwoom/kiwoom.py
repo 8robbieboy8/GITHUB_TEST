baf4f92aac4834ab1a8c0f366657f0eeed5616a3
8robbieboy8 <gyjh486@gmail.com>
2022-05-10 13:00:24 +0900

    stock

--- my_stock/kiwoom/kiwoom.py
+++ my_stock/kiwoom/kiwoom.py
@@ -1,3 +1,29 @@
-class Kiwoom():
+from PyQt5.QAxContainer import *
+
+
+class Kiwoom(QAxWidget):
     def __init__(self):
+        super().__init__()
+        
         print('Kiwoom class 입니다.')
+        
+        self.get_ocx_instance()
+        self.event_slots()
+        
+        self.signal_login_commConnect()
+     
+        
+    def get_ocx_instance(self):
+        self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
+      
+        
+    def event_slots(self):
+        self.OnEventConnect.connect(self.login_slot)
+       
+        
+    def login_slot(self, erroCode):
+        print(erroCode)
+        
+        
+    def signal_login_commConnect(self):
+        self.dynamilcCall("CommConnect()")
